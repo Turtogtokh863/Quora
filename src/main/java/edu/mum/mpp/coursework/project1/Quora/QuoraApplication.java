72f3e77fbafcb7611f93a6fa95d21d382cc763b4
Turtogtokh863 <tmunkhtsooj@mum.edu>
2019-05-02 18:03:21 -0500

    Initial commit

---
+++ src/main/java/edu/mum/mpp/coursework/project1/Quora/QuoraApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.mpp.coursework.project1.Quora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoraApplication.class, args);
+	}
+
+}
